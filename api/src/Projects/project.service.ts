e2cb285b574588cb0f219d30f619e3273ada1939
raulgonca <raulgo2003@gmail.com>
2024-04-09 12:28:56 +0200

    9/4

--- api/src/Projects/project.service.ts
+++ api/src/Projects/project.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
 import { Project } from './project.entity';
 
 
@@ -24,4 +24,31 @@ export class ProjectService {
   async deleteRepo(id: number): Promise<void> {
     await this.projectRepository.delete(id);
   }
+
+  async findOne(id: number): Promise<Project> {
+    throw new Error('Method not implemented.');
+  }
+
+  async updateProject(
+    id: number,
+    nameproject: string,
+    description: string,
+    fechaInicio: Date,
+    fechaFinalizacion: Date,
+  ): Promise<Project> {
+    const project = await this.projectRepository.findOne({ where: { id } });
+
+    if (!project) {
+      throw new Error(`Proyecto con id ${id} no encontrado`);
+    }
+
+    project.nameproject = nameproject;
+    project.description = description;
+    project.fechaInicio = fechaInicio;
+    project.fechaFinalizacion = fechaFinalizacion;
+
+    return this.projectRepository.save(project);
+  }
+
+
 }
